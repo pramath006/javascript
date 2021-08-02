08b2cf0ff521be1ca37f4a939a22eace03dbdc0a
pramath006 <88322565+pramath006@users.noreply.github.com>
2021-08-02 21:18:33 +0530

    Add files via upload

---
+++ data_types2.js
@@ -0,0 +1,17 @@
+function performOperation(secondInteger, secondDecimal, secondString) 
+{
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    console.log(firstInteger + Number(secondInteger));
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    console.log(firstDecimal + Number(secondDecimal));
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);   
+};
+
+const secondInteger = 4;
+const secondDecimal = 4.32;
+const secondString = 'yes';
+performOperation(secondInteger, secondDecimal, secondString);
